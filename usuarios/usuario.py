20ed7d36a4626aa87227176a8ce4253fdcbbd890
Oscarsr10 <oskiitarsr@gmail.com>
2020-07-29 21:14:23 +0200

    Conexion a BD

--- usuarios/usuario.py
+++ usuarios/usuario.py
@@ -1,3 +1,15 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="",
+    db="master_python",
+    port=3306
+)
+
+print(database)
+
 class Usuario:
     
     def __init__(self, nombre, apellidos, email, password):
